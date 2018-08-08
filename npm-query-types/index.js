d34dc46c4975cb74e0b66bdc7a75f12339c7cad6
GeniusK <geniusk1047@naver.com>
2018-08-08 15:31:15 +0900

    주석 제거

--- npm-query-types/index.js
+++ npm-query-types/index.js
@@ -2,7 +2,7 @@ var queryType = require('query-types');
 var express = require('express');
 var app = express()
  
-// app.use(queryType.middleware())
+app.use(queryType.middleware())
  
 app.get('/', function(req, res) {
   res.json(req.query); // Response example: { enabled: true, price: 123.13, name: 'John Smith', origin: [31.412, 41.213] }  
